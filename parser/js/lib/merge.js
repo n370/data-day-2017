a5816ccf22babdddef6d7d99059588a18db2e7d2
Dylson Valente Neto <ama@n370.info>
2017-03-05 11:40:10 -0300

    Read from data files and log the content

---
+++ parser/js/lib/merge.js
@@ -0,0 +1,9 @@
+module.exports = merge;
+
+/**
+ * Módulo para mesclar los geodatos con los metadatos.
+ * @param {Array} dataset Es una lista javascript con dos itenes. 
+ */
+function merge(dataset) {
+  process.stdout.write(dataset)
+}
--- parser/js/main.js
+++ parser/js/main.js
@@ -1,12 +1,26 @@
-var shapefile = require('shapefile')
+var async = require('async')
+var fs = require('fs')
 var csv = require('csv')
+var merge = require('./lib/merge')
 
-shapefile.open("../../data/v_mdg_vias.shp")
-  .then(source => source.read()
-    .then(function log(result) {
-      if (result.done) return;
-      console.log(result.value);
-      return source.read().then(log);
+var paths = ['../../data/metadatos.csv', '../../data/geodatos.geojson']
+
+async.waterfall([
+    readDataFiles,
+    parseCSV
+], (err, dataset) => { merge(dataset) })
+
+function readDataFiles(callback) {
+  async.map(paths, fs.readFile, (err, results) => {
+    callback(null, results)
+  })
+}
+
+function parseCSV(results, callback) {
+  csv.parse(results[0], (err, data) => {
+    callback(null, JSON.stringify({
+      geodatos: results[1],
+      metadatos: data
     }))
-  .catch(error => console.error(error.stack));
-
+  })
+}
